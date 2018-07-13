7e528753e61ba8f86155cdf544417680f1b278fa
michaelbIgbao <michaelliangjiabao@gmail.com>
2018-07-13 00:23:15 -0400

    Add files via upload

---
+++ Tank_Game.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tank_Game, "Tank_Game" );
